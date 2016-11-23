993cf93c30f3a3ab529871be7c3a82e45e3a2b3f
baristaGeek <estebanvargas94@gmail.com>
2016-11-23 01:40:24 -0500

    All the way up to lesson 9

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,19 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+count = 0
 
+for k, v in enron_data.items():
+
+    if v["poi"] == 1:
+
+        count += 1
+
+print count
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,21 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+# to calculate error as specified in the code comment
+    errors = net_worths - predictions
+    cleaned_data =zip(ages, net_worths, errors)
 
-    
-    return cleaned_data
+# the [0] isn't necessary in this case since errors is a 1-D array
+    cleaned_data = sorted(cleaned_data,key=lambda x:x[2], reverse=True)
+    limit = int(len(net_worths) * 0.1)
 
+# cast the iterator object as a list. I needed to do this to avoid errors
+# in the calling code.
+    return list(cleaned_data[limit:])
